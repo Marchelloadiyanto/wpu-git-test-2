b59ef31d817cda38be183f583def359177757585
marchello <hokaigt@gmail.com>
2022-01-20 12:39:54 +0700

    menambahkan file php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git remote</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto fuga laborum architecto voluptatibus libero esse hic. Unde quidem eius laudantium tenetur cum velit expedita, praesentium repudiandae numquam neque odit id!</p>
+</body>
+</html>
